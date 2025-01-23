61252c7b1f784e410be5b63d3fdb555d7d232df1
Linnea <lienlinneakarlsson@gmail.com>
2025-01-23 10:34:56 +0100

    text and images

--- javascript.js
+++ javascript.js
@@ -3,9 +3,3 @@ button.addEventListener("click", () => {
     alert("Tack för ditt meddelande!");
 });
 
-/* 
-const title = document.getElementById("title");
-title.innerHTML = "Hej från Javascript!";
-title.style.color = "red";
-title.style.fontSize = "24px";
-*/
