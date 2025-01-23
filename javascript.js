a7e43883341cbabb8e5e07f8dd02fbc1d4aa746d
Linnea <lienlinneakarlsson@gmail.com>
2025-01-23 04:24:30 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("sendButton");
+button.addEventListener("click", () => {
+    alert("Tack för ditt meddelande!");
+});
+
+/* 
+const title = document.getElementById("title");
+title.innerHTML = "Hej från Javascript!";
+title.style.color = "red";
+title.style.fontSize = "24px";
+*/
